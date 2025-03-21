94082462d57773c28e74874de9898d03abd5ca45
Gerardo Rodrigues <gerardofilho56@gmail.com>
2025-03-21 09:52:39 -0300

    estilos globais

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import { ButtonContainer } from "@/styles/Button";
+import { globalStyles } from "@/styles/global";
+
+globalStyles();
 
 export default function Home() {
   return (
     <>
       <ButtonContainer>Enviar</ButtonContainer>
       <h1>Product</h1>
+      <p>Mesa</p>
     </>
   );
 }
--- src/styles/Button/index.ts
+++ src/styles/Button/index.ts
@@ -1,10 +1,11 @@
 import { styled } from "..";
 
 export const ButtonContainer = styled("button", {
-    backgroundColor: "$primary",
+    backgroundColor: "$green500",
     padding: 10,
     borderRadius: 10,
     cursor: "pointer",
+    border: "none",
 
     "&:hover": {
         backgroundColor: "blue",
