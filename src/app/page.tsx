fb3d087acd63033130128530dfde729bc777cdcc
Gerardo Rodrigues <gerardofilho56@gmail.com>
2025-03-21 11:17:13 -0300

    estrutura da home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,32 @@
 import { Header } from "@/components/header/page";
 import { Container } from "@/styles/pages/app";
+import { HomeContainer, ProductContainer } from "@/styles/pages/home";
+import Image from "next/image";
+
+import camisa1 from "../assets/camisetas/camisa1.png";
+import camisa2 from "../assets/camisetas/camisa2.png";
+import camisa3 from "../assets/camisetas/camisa3.png";
 
 export default function Home() {
   return (
     <Container>
       <Header />
-      <h1>Product</h1>
-      <p>Mesa</p>
+      <HomeContainer>
+        <ProductContainer>
+          <Image src={camisa1} width={520} height={480} alt="" />
+          <footer>
+            <strong>Camiseta X</strong>
+            <span>R$ 79,90</span>
+          </footer>
+        </ProductContainer>
+        <ProductContainer>
+          <Image src={camisa2} width={520} height={480} alt="" />
+          <footer>
+            <strong>Camiseta X</strong>
+            <span>R$ 119,90</span>
+          </footer>
+        </ProductContainer>
+      </HomeContainer>
     </Container>
   );
 }
